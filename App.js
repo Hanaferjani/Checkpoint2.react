b118b39b1d894f4f112261e0bd98e319fcd6dc07
Hanaferjani <Hene.ferjani@gmail.com>
2024-09-11 17:16:43 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Playerslist from './components/Playerslist';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+   <Playerslist/>
+    </div>
+  );
+}
+
+export default App;
